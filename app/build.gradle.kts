ac247d942c904fb404a4792c39820578c61829d0
ryanx30 <muhammadrahadian3005@gmail.com>
2025-09-29 02:09:06 +0700

    Commit No. 2 - Parse JSON with Moshi and Display Image URL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.retrofit)
     implementation(libs.converter.scalars)
+    implementation(libs.converter.moshi)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
